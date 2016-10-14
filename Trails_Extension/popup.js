303544a27781fbffee5bf7a7ff11d7ffb5d01b00
Aman Da Vinci <amanthevinci@gmail.com>
2016-10-14 17:39:05 +0530

    Capture Transition and Database Entry
    Transition type is sent along with history items and the data is entered
    in the database

--- Trails_Extension/popup.js
+++ Trails_Extension/popup.js
@@ -16,19 +16,31 @@ document.getElementById('Trails').onclick = Get_History;
 
 function Get_History() {
 chrome.history.search({text:'',maxResults:3}, function(data) {
-   var json_flow=JSON.stringify(data);
-   //document.write(json_history);
+   var jsonon_flow=JSON.stringify(data);
+     //document.write(json_history);
       d = document;
       var f = d.createElement('form');
       f.action = 'http://localhost/Trails_Server/test.php';
       f.method = 'post';
+
       var i = d.createElement('input');
       i.type = 'hidden';
       i.name = 'flow';
       i.value = json_flow;
       f.appendChild(i);
-      d.body.appendChild(f);
-      f.submit();
+      
+      chrome.history.getVisits({url: data[0]['url']}, function transition(url){
+        var transition_flow=JSON.stringify(url);
+        
+        var i1 = d.createElement('input');
+        i1.type = 'hidden';
+        i1.name = 'transition';
+        i1.value = transition_flow;
+        f.appendChild(i1);
+        
+        d.body.appendChild(f);
+        f.submit();
+      } );
    /*
    request= new XMLHttpRequest();
    request.open("POST", "http://localhost/test.php", true);
--- Trails_Server/test.php
+++ Trails_Server/test.php
@@ -1,15 +1,66 @@
 <?php
+
     if($_POST["flow"])
 	{
-		$url = $_POST["flow"];
+		$json_flow = $_POST["flow"];
 		
 		echo "<h1>Flow Captured:</h1> <br>"; // Success Message
-		var_dump(json_decode($url,true));
+		var_dump(json_decode($json_flow,true));
 
-		$history = json_decode($url,true);
-		echo "<hr>".$history[0]['title']."<br>".$history[0]['url'];
-		echo "<hr>".$history[1]['title']."<br>".$history[1]['url'];
-		echo "<hr>".$history[2]['title']."<br>".$history[2]['url']."<hr>";
+		$history = json_decode($json_flow,true);
+		echo "<hr>".$history[0]['title']."<br>".$history[0]['url']."<br>".$history[0]['id'];
+		echo "<hr>".$history[1]['title']."<br>".$history[1]['url']."<br>".$history[1]['id'];
+		echo "<hr>".$history[2]['title']."<br>".$history[2]['url']."<br>".$history[2]['id']."<hr>";
+
+		// Store the data
+		for ($i = 0; $i < 3; $i++)
+		{
+			$servername = "localhost";
+			$username = "root";
+			$password = "test123";
+			$dbName = "Trails";
+
+			$conn = new mysqli($servername, $username, $password, $dbName);
+			if ($conn->connect_error)
+			{
+				die("Connection failed: ".$conn->connect_error);
+			}
+
+			$id = mysqli_real_escape_string($conn, $history[$i]['id']);
+			$url = mysqli_real_escape_string($conn, $history[$i]['url']);
+			$title= mysqli_real_escape_string($conn, $history[$i]['title']);
+			$lastVisitTime = mysqli_real_escape_string($conn, $history[$i]['lastVisitTime']);
+			$typedCount = mysqli_real_escape_string($conn, $history[$i]['typedCount']);
+			$visitCount = mysqli_real_escape_string($conn, $history[$i]['visitCount']);
+			
+			$sql = "INSERT INTO FlowDB (id, url, title, lastVisitTime, typedCount, visitCount)
+					VALUES ('".$id."', '".$url."','".$title."','".$lastVisitTime."','".$typedCount."','".$visitCount."')";
+
+			if ($conn ->query($sql) === TRUE)
+			{
+				echo "New record created";
+			}
+			else
+			{
+				echo "Error: ".$sql."<br>".$conn->error;
+			}
+
+			$conn->close();	
+		}
 
 	}
+	
+	if($_POST["transition"])
+	{
+		$url = $_POST["transition"];
+		
+		echo "<h1>transition:</h1> <br>"; // Success Message
+		var_dump(json_decode($url,true));
+
+		$transition = json_decode($url,true);
+		echo "<hr>".$transition[0]['id']."<br>".$transition[0]['referringVisitId'];
+		
+	}
+	
+	$flow = array_merge($transition, $history);
 ?>
